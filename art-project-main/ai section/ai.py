ab05fb67cc5d3265f7676d0efe10370348c898d0
ymmot239 <pacman239sa@gmail.com>
2024-01-12 12:15:57 -0600

    ai changes

---
+++ art-project-main/ai section/ai.py
@@ -0,0 +1,70 @@
+from os import listdir
+import os
+import win32com.client
+from os.path import isfile, join
+import math
+import random
+
+genres = {}
+user_change = {}
+music_list = {}
+
+
+def get_genres():
+    with open("genre_list.txt","r") as f:
+        for x in f.readlines():      
+            genres[x.split(":")[0]] = int(x.split(":")[1][:-1])
+    return genres
+
+
+def get_return():
+    with open("user_list.txt","r") as f:
+        for x in f.readlines():        
+            user_change[x.split(":")[0]] = int(x.split(":")[1][:-1])
+    return user_change
+
+
+def edit_weights(original_weight, new_weight):
+    change  = {}
+    sum = 0
+    for key in original_weight:
+        change[key] = (new_weight[key] - original_weight[key])/2
+        original_weight[key] = original_weight[key] + (new_weight[key] - original_weight[key])/2
+        sum += original_weight[key]
+    #print(original_weight)
+    return original_weight
+    
+
+def get_songs():
+    with open("genre_to_song.txt","r") as f:
+        for x in f.readlines(): 
+            part = x.split(" : ")
+            if part[0] in music_list:
+                music_list[part[0]].append([part[1],int(part[2])])
+            else:
+                music_list[part[0]] = [[part[1],int(part[2])]]
+    return music_list
+
+
+def new_songs(song_list, genre_weights):
+    songs = random.choices(list(genre_weights.keys()),weights = genre_weights.values(), k=51)
+    songs.sort()
+    number = {i:songs.count(i) for i in songs}
+    selected = []
+    for genre in number:
+        if genre in song_list:   
+            if number[genre] < len(song_list[genre]):
+                holder = {}
+                #while len(selected) < sum+number[genre]:
+                selected.extend(random.sample(song_list[genre], number[genre]))
+            else:
+                selected.extend(song_list[genre])
+                
+    #print(len(selected))
+    return selected
+    
+
+if __name__ == "__main__":
+    #print(edit_weights(get_genres(),get_return()))
+    #print(get_songs())
+    print(new_songs(get_songs(),edit_weights(get_genres(),get_return())))
---
+++ art-project-main/ai section/main.py
@@ -0,0 +1,68 @@
+from os import listdir
+import os
+import win32com.client
+from os.path import isfile, join
+import re, random
+
+def get_file_metadata(path, filename, metadata, index):
+    # Path shouldn't end with backslash, i.e. "E:\Images\Paris"
+    # filename must include extension, i.e. "PID manual.pdf"
+    # Returns dictionary containing all file metadata.
+    sh = win32com.client.gencache.EnsureDispatch('Shell.Application', 0)
+    ns = sh.NameSpace(path)
+
+    # Enumeration is necessary because ns.GetDetailsOf only accepts an integer as 2nd argument
+    file_metadata = dict()
+    item = ns.ParseName(str(filename))
+    print(item)
+    for ind, attribute in enumerate(metadata):
+        attr_value = ns.GetDetailsOf(item, index)
+        if attr_value:
+            file_metadata[attribute] = attr_value
+
+    return file_metadata
+
+def read_new(file_name):
+    file = open(file_name,"r")
+    metadata = ['Genre']
+    holder = file.readlines()
+    path = 'C:\\Users\\pacma\\Downloads\\art\\docker-art-project\\art-project-main\\music\\Classical Guitar'
+    for x in holder:
+        print(get_file_metadata(path,x[:-1],metadata, 16))
+    
+    return holder
+    
+def get_music():
+    mypath = "C:\\Users\\pacma\\Downloads\\art\\docker-art-project\\art-project-main\\music"
+    filelist = next(os.walk(mypath))[1]
+    onlyfiles = {}
+    for x in filelist:
+        newpath = mypath + "/" + x
+        #onlyfiles += [newpath + "/" + f for f in listdir(newpath) if isfile(join(newpath, f))]
+        for f in listdir(newpath):
+             if isfile(join(newpath,f)):
+                 f_edit = re.sub(r'[^a-zA-Z\d\s:]',"",f[:-4]).strip()
+                 if len(f_edit) > 30:
+                     f_edit = f_edit[:30] + "~"
+                 onlyfiles[f_edit] = [newpath+"/"+f,"Classical Guitar"]
+    return onlyfiles
+
+def recommend_number():
+    pass
+
+def export_changes():
+    pass
+
+
+if __name__ == "__main__":
+    read_new("test.txt")
+    List = get_music()
+    file = open("genre_to_song.txt", "w+", encoding="utf-8")
+    for key in List:
+        file.write(List[key][1]+" : "+key+"\n")
+    file.close()
+    file = open("song_to_path.txt", "w+", encoding="utf-8")
+    for key in List:
+        file.write(key + " : " + List[key][0] + "\n")
+
+    
